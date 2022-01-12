b280ec29996cf166f1980e968e596d4bc195aa73
natividadesusana <susanajdsn@gmail.com>
2022-01-12 18:02:45 -0300

    iniciando cadastro

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CadastroAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
